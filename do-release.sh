25d3b6bc4e8851b508a0e66a1b4f0fe7f1c147a6
Robin H. Johnson <robbat2@gentoo.org>
2009-02-27 23:15:45 -0800

    Include the name of the new patchset tarball.

--- do-release.sh
+++ do-release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 DATE=$(date +%Y%m%d-%H%MZ -u)
 TAG=mysql-extras-$DATE
+outfile=${TAG}.tar.bz2
 git tag $TAG
-git archive --prefix="${TAG}/" $TAG | bzip2 >../${TAG}.tar.bz2
+git archive --prefix="${TAG}/" $TAG | bzip2 >../${outfile}
+echo "$outfile generated."
