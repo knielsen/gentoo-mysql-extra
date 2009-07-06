89dbf609d084eb353c048afa499aba953949f102
Robin H. Johnson <robbat2@gentoo.org>
2009-07-06 01:10:00 -0700

    Need to use 5 digits of numbering now. LOTS of patches to track.

--- show-patches.pl
+++ show-patches.pl
@@ -18,7 +18,7 @@ my $PV = $ARGV[1];
 chomp $PV;
 
 my ($FH, $index);
-open $FH,'<',"0000_index.txt";
+open $FH,'<',"00000_index.txt";
 my @index = parseIndex($FH);
 my @newindex = selectPatches(\@index, $PN, $PV);
 #print Dumper(@index);
